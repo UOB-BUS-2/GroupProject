961b636e97fba9956d213acc4ce28510125e6608
George Edward Gudgeon <geg@libra>
2026-02-23 17:16:04 +0000

    slight changes to log_food form, made default None/please select

--- app/forms.py
+++ app/forms.py
@@ -13,12 +13,12 @@ from wtforms.widgets.core import CheckboxInput
 class LogMeal(FlaskForm):
     # Maybe added later
     #meal_of_day = SelectField("Log a meal", validators=[DataRequired()], choices = [('breakfast', 'Breakfast'), ('lunch', 'Lunch'), ('dinner', 'Dinner'), ('snack', 'Snack')])
-    protein_selected = SelectField("What was your main source of protein", validators=[DataRequired()],
-                                         choices=[('beef', 'Beef'),('chicken', 'Chicken'),('eggs', 'Eggs'),('tofu', 'Tofu'), ('fish', 'Fish and seafood')])
-    carb_selected = SelectField("What was your main carbohydrate", validators=[DataRequired()],
-                                         choices=[('bread', 'Bread'),('rice', 'Rice'),('pasta', 'Pasta')])
-    veg_selected = SelectField("What was your main vegetable", validators=[DataRequired()],
-                                         choices=[('potato', 'Potato'),('carrot', 'Carrot'),('broccoli', 'Broccoli'),('spinach', 'Spinach'), ('onion', 'Onion')])
+    protein_selected = SelectField("What was your main source of protein", validators=[DataRequired('Please make a selection')],default = None,
+                                         choices=[(None, '--- Select Protein ---'), ('beef', 'Beef'),('chicken', 'Chicken'),('eggs', 'Eggs'),('tofu', 'Tofu'), ('fish', 'Fish and seafood')])
+    carb_selected = SelectField("What was your main carbohydrate", validators=[DataRequired()],default = None,
+                                         choices=[(None, '--- Select Carbohydrate ---'), ('bread', 'Bread'),('rice', 'Rice'),('pasta', 'Pasta')])
+    veg_selected = SelectField("What was your main vegetable", validators=[DataRequired()], default = None,
+                                         choices=[(None, '--- Select Vegetable ---'), ('potato', 'Potato'),('carrot', 'Carrot'),('broccoli', 'Broccoli'),('spinach', 'Spinach'), ('onion', 'Onion')])
     # Maybe added later
     # date_completed = DateField(
     #     "When did you have this?",
