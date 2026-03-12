42daa89a501b9789716cebde23bac545ea097eb1
virajkc1 <virajkc1@gmail.com>
2026-03-12 16:51:56 +0000

    Home

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)   # ✅ remove custom static/template paths
+app = Flask(__name__)   # remove custom static/template paths
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
